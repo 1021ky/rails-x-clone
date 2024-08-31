f64adee13f10eeedd1e01e8a61dc22f175a9b65c
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-08-31 20:05:20 +0900

    バックエンドアプリだとわかるようにディレクトリを移動する

---
+++ backend/lib/rails/x/clone.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "clone/version"
+
+module Rails
+  module X
+    module Clone
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
