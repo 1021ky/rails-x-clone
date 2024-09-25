a50967b8b4eccff38614b869df2e6d97e2d1f200
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-09-25 09:50:20 +0900

    ルーティングの決め方とビューの入れ方を学び追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 名前空間の名前を定義すると、URLのパスにその名前空間が追加される
+  namespace :api do
+    # #の前がコントローラー名、#の後がアクション名
+    # /apiでアクセスすると、Api::TopControllerのindexアクションが呼び出される
+    root "top#index"
+  end
+
+  namespace :admin do
+    root "top#index"
+  end
+
+  namespace :tweet do
+    root "top#index"
+  end
 end
