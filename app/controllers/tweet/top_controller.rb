1858c39560bd32b49b44064bc4f294070d3d3f2b
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2024-10-09 09:00:52 +0900

    APIを書いてみる。がルーティングエラー

--- app/controllers/tweet/top_controller.rb
+++ app/controllers/tweet/top_controller.rb
@@ -1,6 +1,25 @@
 # typed: true
+
+#
+# Tweet::TopController
+#
 class Tweet::TopController < ApplicationController
   def index
-    render action: "index"
+    render action: 'index'
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_create_params)
+    if @tweet.save
+      redirect_to tweet_top_index_path
+    else
+      render action: 'index'
+    end
+  end
+
+  private
+
+  def tweet_create_params
+    params.require(:tweet).permit(:x_user_id, :content)
   end
 end
